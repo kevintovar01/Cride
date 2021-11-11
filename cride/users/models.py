8ad530225b57f7a9d1f5a625f2913cbaad9c7012
kevin <kevinetl01@gmail.com>
2021-11-11 15:19:00 -0500

    modificacion del modelo

--- cride/users/models.py
+++ cride/users/models.py
@@ -13,7 +13,37 @@ from cride.utils.models import CRideModel
 class User(CRideModel, AbstractUser):
     """Usermodel.
 
+
+        Extend  from Django's Abstract User, change the username field
+        to email and add some filed
     
     
     """
-    pass
+    email = models.EmailField(
+        'email address',
+        unique=True,
+        error_messages={
+            'unique':'A user with that email already exists'
+        }
+    )
+
+    phone_number = models.CharField(max_length=17, blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'firts_name', 'last_name']
+
+    is_client = models.BooleanField(
+        'client status',
+        default=True,
+        help_text=(
+            'Help easily distinguish users and perform quieries.'
+            'Clients are the main type of user.'
+        )
+    )
+
+    is_verified = models.BooleanField(
+        'verified',
+        default=True,
+        help_text='Set to true when the user have verified its email address'
+
+    )
