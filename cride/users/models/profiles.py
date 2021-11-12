a9816c4c90d7931038fca77ab4ece11eae0ce122
kevin <kevinetl01@gmail.com>
2021-11-11 19:49:59 -0500

    Profile modeld

---
+++ cride/users/models/profiles.py
@@ -0,0 +1,39 @@
+"""Profile model"""
+
+#Django
+from django.db import models
+
+#Utilities
+from cride.utils.models import CRideModel
+
+
+class Profile(CRideModel):
+    """Profile model
+    
+    A profile holds a user's public data like biography, picture, 
+    and statistics 
+    """
+
+    user = models.OneToOneField('users.User', on_delete=models.CASCADE)
+
+    picture = models.ImageField(
+        'profile picture',
+        upload_to='users/picture/',
+        blank=True,
+        null=True
+    )
+
+    biography = models.TextField(max_length=500, blank=True)
+
+    #Stats
+    rides_token = models.PositiveIntegerField(default=0)
+    rides_offered = models.PositiveIntegerField(default=0)
+    reputation = models.FloatField(
+        default=5.0,
+        help_text="User's reputation based on the rides token and offered  "
+    )
+
+
+    def __str__(self):
+        """return user's str representation"""
+        return str(self.user)
